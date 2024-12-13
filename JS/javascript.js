9fdfd9c02b4fa41c713ba6cd5d0d2ccd5eedf006
MirkoDD <mirkodidio111@gmail.com>
2024-12-13 18:08:21 +0100

    added:javascript(file),form,buttons_and_pinned_all_useful_id_from_index-fixed:minor_details

---
+++ JS/javascript.js
@@ -0,0 +1,17 @@
+const userElm = document.getElementById("name")
+const kmElm = document.getElementById("km")
+const ageElm = document.getElementById("age")
+
+const submittedElm = document.getElementById("submitted")
+const submitterElm = document.getElementById("submitter")
+const undoElm = document.getElementById("undo")
+
+
+submittedElm.addEventListener("submit", (sub) => {
+    sub.preventDefault()
+
+})
+
+undoElm.addEventListener("click", () => {
+
+})
